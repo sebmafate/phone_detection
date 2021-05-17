5b6ad116b601576c9d2a11b5fca68b2bdeaffae8
Benoit <benoit5672@yahoo.fr>
2021-05-17 17:46:05 +0200

    Update desktop files to match 4.1 and 4.2 look and feel

--- desktop/js/phone_detection.js
+++ desktop/js/phone_detection.js
@@ -1,40 +1,53 @@
+/*
+* Permet la réorganisation des commandes dans l'équipement
+*/
+$("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+
+/*
+* Fonction permettant l'affichage des commandes dans l'équipement
+*/
 function addCmdToTable(_cmd) {
-    // Test si le paramètre a été défini
-    if (!isset(_cmd)) {
-        // Initialise _cmd
-        _cmd = {configuration: {}};
-    }
-    var row = ' \
-            <tr class="cmd" data-cmd_id="' + init(_cmd.id) + '"> \
-              <td> \
-                <input class="cmdAttr form-control input-sm" data-l1key="id" style="display : none;"> \
-                <input class="cmdAttr form-control input-sm" data-l1key="name"> \
-              </td> \
-              <td> \
-                <span>\
-                <label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label> \
-              </span> \
-              </td> \
-              <td>\
-                <a class="btn btn-default btn-xs cmdAction expertModeVisible" data-action="configure"><i class="fa fa-cogs"></i></a> \
-                <a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a> \
-                <!--<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i>--> \
-              </td> \
-            </tr>';
-    // Ajoute la ligne au tableau
-    $('#table_cmd tbody').append(row);
-    // Ajoute l'identifiant de la commande à la balise <tr> de la ligne
-    $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
-    // Initialise les fonctionnalités de la ligne
-    jeedom.cmd.changeType($('#table_cmd tbody tr:last'), init(_cmd.subType));
-
-
-  //   <td> \
-  //   <span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span> \
-  //   <span class="subType" subType="' + init(_cmd.subType) + '"></span> \
-  // </td> \
-
-}
+  if (!isset(_cmd)) {
+     var _cmd = {configuration: {}};
+   }
+   if (!isset(_cmd.configuration)) {
+     _cmd.configuration = {};
+   }
+   var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
+   tr += '<td style="min-width:50px;width:70px;">';
+   tr += '<span class="cmdAttr" data-l1key="id"></span>';
+   tr += '</td>';
+   tr += '<td style="min-width:400px;width:500px;">';
+   tr += '<div class="row">';
+   tr += '<div class="col-xs-7">';
+   tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom de la commande}}">';
+   tr += '</div>';
+   tr += '</div>';
+   tr += '</td>';
+   tr += '<td style="min-width:120px;width:140px;">';
+   tr += '<div><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></div> ';
+   tr += '</td>';
+   tr += '<td>';
+   if (is_numeric(_cmd.id)) {
+     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
+     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>';
+   }
+   tr += '</tr>';
+   $('#table_cmd tbody').append(tr);
+   var tr = $('#table_cmd tbody tr').last();
+   jeedom.eqLogic.builSelectCmd({
+     id:  $('.eqLogicAttr[data-l1key=id]').value(),
+     filter: {type: 'info'},
+     error: function (error) {
+       $('#div_alert').showAlert({message: error.message, level: 'danger'});
+     },
+     success: function (result) {
+       tr.find('.cmdAttr[data-l1key=value]').append(result);
+       tr.setValues(_cmd, '.cmdAttr');
+       jeedom.cmd.changeType(tr, init(_cmd.subType));
+     }
+   });
+ }
 
 
 $('#bt_remote_phone_detection').on('click', function () {
